8b7b8bc5be05f456ab0e50bf7e864fe1a7fd871e
DESKTOP-TLVNNT3\Alex <alejandro.rg91@gmail.com>
2024-05-04 11:34:18 +0200

    ARG - Prueba de disabled

--- src/prueba4.py
+++ src/prueba4.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from tkinter.ttk import Scale, Label
 
 import ttkbootstrap as ttk
@@ -6,11 +7,31 @@ from ttkbootstrap.constants import *
 root = ttk.Window()
 
 
+def update_value(val):
+    text_var.set(val)
+
 
 # default Scale style
-scale = Scale(bootstyle="info", orient="vertical")
+#scale = Scale(bootstyle="info", orient="vertical", command=update_value, from_=0, to=10)
+scale = tk.Scale(root, orient="vertical", command=update_value, from_=0, to=10)
 scale.pack()
 
-label = Label()
+text_var = tk.StringVar()
+text_var.set("0,1")
+
+label = Label(root, textvariable=text_var)
+label.pack()
+
+#scale.config(state=tk.DISABLED)
+
+
+def action_update():
+    scale.config(state=tk.DISABLED)
+
+
+# Botón para ejecutar la función
+button = tk.Button(root, text="Get Scale Value", command=action_update)
+button.pack(anchor="center")
+
 
 root.mainloop()
